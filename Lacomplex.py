f01f2315df3ec75a00c2bf38bbd244af4c738db7
BFYFlorence <mrcaofan@163.com>
2020-09-23 08:51:12 +0800

    ~

--- Lacomplex.py
+++ Lacomplex.py
@@ -1,8 +1,8 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras import layers, optimizers, datasets
 import datetime
 import tensorboard
@@ -10,8 +10,9 @@ import os
 import re
 
 np.set_printoptions(suppress=True)  # 取消科学计数显示
-# np.set_printoptions(threshold=np.inf)
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+np.set_printoptions(threshold=np.inf)
+os.environ['KMP_DUPLICATE_LIB_OK']='True'       # Macos需要设为true
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 # os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 
@@ -19,42 +20,47 @@ class Lacomplex:
     def __init__(self):
         self.contact_dis = 4.5  # 重原子之间的contact距离
 
-        self.startFrame = 0  # 首帧                         batchFrame2Dis       ConDifPerFra    extractFeat
-        self.endFrame = 0 + 1  # 末帧                        batchFrame2Dis                ConDifPerFra   extractFeat
-        self.set_name = 1  # 字典存储名称, 用于set和freq的存储    batchFrame2Dis
+        self.startFrame = 1  # 首帧                         batchFrame2Dis       ConDifPerFra    extractFeat
+        self.endFrame = 5000 + 1  # 末帧                        batchFrame2Dis                ConDifPerFra   extractFeat
+        self.set_name = 7  # 字典存储名称, 用于set和freq的存储    batchFrame2Dis
 
         # batchFrame2Dis     mergeSet    ConDifPerFra    averageDis    numperaa     aveDistribution
         # extractFeat
-        self.csv_path = "./"  # 表格读取路径
-        self.frame_path = "./"  # 存储每一帧的文件夹             batchFrame2Dis
+        self.csv_path   = "/home/liuhaiyan/fancao/work/cut_DNABindDomain/Iptg_ProNoBindDna/test/md/csv/"  # 表格读取路径
+        self.frame_path = "/home/liuhaiyan/fancao/work/cut_DNABindDomain/Iptg_ProNoBindDna/test/md/frame/"  # 存储每一帧的文件夹             batchFrame2Dis
+        self.NNread     = "./"
 
         self.startSet = 1  # 字典起始                           mergeSet
-        self.endSet = 1+1  # 字典终止                           mergeSet
+        self.endSet = 10+1  # 字典终止                           mergeSet
 
 
         self.Interval = 1  # 取帧间隔,看帧的名称
         self.Numset = 10  # 保存的集合文件数量
-        self.train_data_name = "Nobind"                      # extractFeat
+        self.train_data_name = "iptg_nobind"                      # extractFeat
                                                         # batchFrame2Dis
 
         self.frame_name = "md{0}.pdb"  # 每一帧的名称
         self.csv_name = "{0}.csv"  # 每一张表的名称
 
-        self.fig_save_path = "/Users/erik/Desktop/stat/NoIptg_ProNoBindDna/png/{0}.png"  # 图片保存路径
+        # self.fig_save_path = "/Users/erik/Desktop/stat/NoIptg_ProNoBindDna/png/{0}.png"  # 图片保存路径
 
-        self.vmd_rmsd_path = "/Users/erik/Desktop/work/consider_tem/NoIptg_ProNoBindDna/"    # rmsd
-        self.rmsd_name = "restrain.dat"     # rmsd
+        self.vmd_rmsd_path = "/Users/erik/Desktop/work/cut_DNABindDomain/Iptg_ProNoBindDna/"    # rmsd
+        self.stat_name = "potential.xvg"     # rmsd
 
         # self.csv_save_path = "/home/liuhaiyan/fancao/work/cut_DNABindDomain/NoIptg_ProNoBindDna/md/csv/"
         # self.csv_read_path = "/home/liuhaiyan/fancao/work/cut_DNABindDomain/NoIptg_ProNoBindDna/md/csv/"
         # self.csv_save_path = "/home/liuhaiyan/fancao/work/cut_DNABindDomain/NoIptg_ProBindDna/md/csv/"  # 表格保存路径
 
         # extractFeat
-        self.NNread = "./"
 
+    def processIon(self, aa):   # 处理质子化条件
+        if aa in ['ASP', 'ASH']:
+            return 'ASP'
+        if aa in ['HIS', 'HIE', 'HID', 'HIP']:
+            return 'HIS'
+        return aa
 
-    @staticmethod
-    def readHeavyAtom(path) -> np.array:
+    def readHeavyAtom(self, path) -> np.array:
         # 读取每条链重原子的坐标
         """[[-21.368 108.599   3.145]
             [-19.74  109.906   6.386]
@@ -85,14 +91,13 @@ class Lacomplex:
 
                 serial = record[6:11].strip()  # 697
                 atname = record[12:16].strip()  # CA
-                resName = lc.processIon(record[17:20].strip())  # PRO
+                resName = self.processIon(record[17:20].strip())  # PRO
                 current_chain = record[21].strip()  # 获取chain_ID,A
                 resSeq = record[22:26].strip()  # 3
                 cor_x = record[30:38].strip()  # Å
                 cor_y = record[38:46].strip()
                 cor_z = record[46:54].strip()
                 element = record[76:78].strip()  # C
-
                 if atom == "TER" or (current_chain != chain and index):  # 检测chain_ID的变化
                     # 轨迹文件导出时没有TER行，所以由
                     # or后面条件辨别
@@ -180,7 +185,6 @@ class Lacomplex:
         data_df = pd.read_csv(path)
         b_atom_nam = data_df[data_df.columns[0]]
 
-
         for i in range(self.startFrame, self.endFrame):  # 读取每一帧对应的原子距离csv
             path = self.csv_path + self.csv_name.format(i * self.Interval)  # 生成对应csv路径
             data_df = pd.read_csv(path)
@@ -194,7 +198,7 @@ class Lacomplex:
             data_df.to_csv(self.csv_path + self.csv_name.format("contact_dif"), float_format="%.5f", )
 
     def averageDis(self):
-        path = self.csv_path + self.csv_name.format("contact_dif")  # contact_dif
+        """path = self.csv_path + self.csv_name.format("contact_dif")  # contact_dif
         contact_dif = pd.read_csv(path)
         ave_array = np.zeros(shape=(len(contact_dif.columns)-1,1))
         for i in range(1, len(contact_dif.columns)):
@@ -203,7 +207,8 @@ class Lacomplex:
         ave_df.columns = ["ave"]
         ave_df.index = contact_dif.columns[1:]
         #                                                   average_dif
-        ave_df.to_csv(self.csv_path + self.csv_name.format("average_dif"), float_format="%.5f", )
+        ave_df.to_csv(self.csv_path + self.csv_name.format("average_dif"), float_format="%.5f", )"""
+        
 
     def numperaa(self):        # average_dif
         path = self.csv_path + "whole_cp.npy"
@@ -256,39 +261,80 @@ class Lacomplex:
         ax1.plot(frame,rms)
         plt.show()
 
-    def processIon(self, aa):   # 处理质子化条件
-        if aa in ['ASP', 'ASH']:
-            return 'ASP'
-        if aa in ['HIS', 'HIE', 'HID', 'HIP']:
-            return 'HIS'
-        return aa
+    def statistics(self):
+        target_file = self.vmd_rmsd_path + self.stat_name
+        indicator = ['potential(kJ/mol)', 'temperature', 'pressure', 'density', 'RMSD-CA', 'RMSF(nm)'][0]
+        #                   0                     1           2         3         4          5
+        x=[]
+        y=[]
+        with open(target_file) as f:
+            for i in f.readlines():
+                i = i.strip()
+                if len(i) == 0:  # 遇见空行，表示迭代至文件末尾，跳出循环
+                    break
+                if i[0] != "#" and i[0] != "@":
+                    li = i.split()
+                    x.append(float(li[0]))
+                    y.append(float(li[1]))
+
+        fig = plt.figure(num=1, figsize=(15, 8), dpi=80)
+        ax1 = fig.add_subplot(1, 1, 1)
+        ax1.set_title('Figure', fontsize=20)
+        ax1.set_xlabel('frame', fontsize=20)
+        ax1.set_ylabel(indicator, fontsize=20)
+        ax1.plot(range(len(x)),y)
+        plt.show()
+
+    def aa_contact(self):       # whole_cp
+        path = self.csv_path + "total_contact.npy"
+        cp = list(np.load(path, allow_pickle=True).item())
+        cp.sort()
+        aa_cp = set()
+        for i in cp:
+            record1 = i[0].split('-')
+            record2 = i[1].split('-')
+            aa_cp.add((record1[0]+'-'+record1[1], record2[0]+'-'+record2[1]))
+        print(len(aa_cp))
+        np.save(self.csv_path + "aa_contact.npy", aa_cp)
 
     def extractFeat(self):
         train_data = []
         # contact_li都是原始状态的残基名称，不再有ASH、HID等质子化名称 whole_cp
-        contact_li = list(np.load(self.NNread + "total_contact.npy", allow_pickle=True).item())
+        #                                        total_contact
+        contact_li = list(np.load(self.NNread + "aa_contact.npy", allow_pickle=True).item())
         contact_li.sort()  # 保证contact_pair顺序一致
-        # patern = re.compile("'(.*?)'")
-        # print(contact_li)
 
         for i in range(self.startFrame, self.endFrame):
             csv_path = self.csv_path + self.csv_name.format(i * self.Interval)
             print("Reading",csv_path)
             dis_df = pd.read_csv(csv_path)
-            print(dis_df)
-            row = dis_df[dis_df.columns[0]]
-            dis_df.index = row
-            # dis_df.columns = new_col
+
+            # 删除原子序号信息，便于随机访问
+            new_col = ['Unnamed: 0']
+            new_index = []
+            for col in dis_df.columns[1:]:
+                record = col.split('-')
+                new_col.append(record[0] + '-'+record[1]+'-'+record[2])
+
+            for index in dis_df[dis_df.columns[0]]:
+                record = index.split('-')
+                new_index.append(record[0]+'-'+record[1]+'-'+record[2])
+
+            dis_df.columns = new_col
+            dis_df.index = new_index
 
             feat = []
             for cp in contact_li:
-                # print(cp)
-                # if cp[0][:3] != '109': and cp[1][:3] != '109':   # 两个初始结构在109位的残基不同
-                # if cp[0].split('-')[2]=='CB' and cp[1].split('-')[2]=='CB':
-                feat.append(dis_df[cp[0]][cp[1]])   # 先列后行
+                print(cp)
+
+                # 因为gly没有CA，所以换为CA
+                a_index = cp[0]+'-'+ ['CB' if cp[0][-3:]!='GLY' else 'CA'][0]
+                b_index = cp[1]+'-'+ ['CB' if cp[1][-3:]!='GLY' else 'CA'][0]
+                feat.append(dis_df[a_index][b_index])   # 先列后行
             train_data.append(feat)  # 文件csv和数组索引差1
+
         train_array_x = np.array(train_data)
-        # print(train_array_x)
+        print(train_array_x)
         np.save(self.NNread + "{0}.npy".format(self.train_data_name), train_array_x)
 
     def norm(self, data):
@@ -303,90 +349,121 @@ class Lacomplex:
     def mergetrain(self):
         bind = np.load("./Bind.npy", allow_pickle=True)
         nobind = np.load("./Nobind.npy", allow_pickle=True)
-        train = np.vstack((bind, nobind))
-        print(bind, nobind, train)
+        train = np.vstack((bind[500:], nobind[500:]))
+        # print(bind, nobind, train)
         np.save("./train.npy", train)
 
+    def calaccrate(self, model, data_x, vali, info):
+        out = model(data_x)
+        result = [0 if i[0] > i[1] else 1 for i in out]
+        error_num = 0
+        for p in range(len(result)):
+            if result[p] != vali[p]:
+                error_num += 1
+
+        error_rate = error_num / len(result)
+        print(info, error_rate * 100, "%")
+        return error_rate
 
     def train(self, train_bool=None):
-        path = self.NNread + "CP_ALL/train.npy"
-        data = np.load(path, allow_pickle=True)    # 前1500是Bind，后1500是Nobind, (3000, 9394)
-        # print(data[0])
+        for i in range(1,31):               # 批量训练神经网络
+            path = self.NNread + "twostates_train.npy"
+            data = np.load(path, allow_pickle=True)  # 前4500是Bind，后4500是Nobind
+            # print(data[0])
+            num_classes = 2
+            batch_size = 20
+            valid_batch_size = 128
+            shuffle_buffer = 10000
+            NUM_EPOCHS = 10000
+            models_path = './models/{0}/'.format(i)  # saver保存路径
+            logs_dir = './logs/{0}/'.format(i)
+            logs_train_dir = os.path.join(logs_dir, "train")
+            logs_valid_dir = os.path.join(logs_dir, "valid")
 
-        label = np.zeros(shape=(data.shape[0]))
-        label[:4500] = 1
-        # print(label.shape)
-        # train
-        train_x = np.vstack((data[:4000],data[4500:8500]))
-        lc.norm(train_x)
 
-        train_y = np.hstack((label[:4000],label[4500:8500]))
+            for dir_name in [logs_dir, logs_train_dir, logs_valid_dir, models_path]:
+                if not os.path.exists(dir_name):
+                    os.mkdir(dir_name)
 
-        train_x = tf.convert_to_tensor(train_x, dtype=tf.float32)
-        train_y = tf.convert_to_tensor(train_y, dtype=tf.int32)
+            label = np.zeros(shape=(data.shape[0]))
+            label[:4500] = 1
+            # train
+            train_x = np.vstack((data[:4000], data[4500:8500]))
+            self.norm(train_x)
+            train_y = np.hstack((label[:4000], label[4500:8500]))
+            train_x = tf.convert_to_tensor(train_x, dtype=tf.float32)
+            train_y = tf.convert_to_tensor(train_y, dtype=tf.int32)
+            train_y_onehot = tf.one_hot(train_y, depth=num_classes)
+            train = tf.data.Dataset.from_tensor_slices((train_x, train_y_onehot))
+            train = train.shuffle(shuffle_buffer)
+            train = train.batch(batch_size)
+            train = train.repeat(NUM_EPOCHS)
+            iterator = iter(train)
 
-        train_y = tf.one_hot(train_y, depth=2)
-        # print(train_x)
-        # print(train_y)
+            # validation
+            valid_x = np.vstack((data[4000:4500], data[8500:]))
+            valid_y = np.hstack((label[4000:4500], label[8500:]))
+            valid_x = tf.convert_to_tensor(valid_x, dtype=tf.float32)
+            valid_y = tf.convert_to_tensor(valid_y, dtype=tf.int32)
+            valid_y_onehot = tf.one_hot(valid_y, depth=num_classes)
+            valid = tf.data.Dataset.from_tensor_slices((valid_x, valid_y_onehot))
+            valid = valid.batch(valid_batch_size)
+            val_iterator = iter(valid)
 
-        # validation
-        valid_x = np.vstack((data[4000:4500], data[8500:]))
-        valid_y = np.hstack((label[4000:4500], label[8500:]))
+            if train_bool:
+                # model
+                # 3800+ features
+                # model = keras.models.load_model("./model.5h")
+                model = tf.keras.Sequential([           # 加个tf.就可以正常保存了！！！另外说一句，keras比tf慢了不止一点
+                    layers.Dense(128, activation='relu'),
+                    layers.Dense(64, activation='relu'),
+                    layers.Dense(16, activation='relu'),
+                    layers.Dense(8, activation='relu'),
+                    layers.Dense(2)
+                ])
+                # log_dir = "./train_log/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
-        if train_bool:
-            # model
-            # 3800+ features
-            model = keras.Sequential([
-                layers.Dense(1024, activation='relu'),
-                layers.Dense(256, activation='relu'),
-                layers.Dense(64, activation='relu'),
-                layers.Dense(8, activation='relu'),
-                layers.Dense(2)
-            ])
-            log_dir = "/Users/erik/PycharmProjects/Lacomplex/train_log/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-            summary_writer = tf.summary.create_file_writer(log_dir)
-            optimizer = optimizers.Adam(learning_rate=0.00001)
+                model.compile()
 
-            train = tf.data.Dataset.from_tensor_slices((train_x, train_y)).batch(32).shuffle(32)
-            itrator = iter(train)
+                summary_writer = tf.summary.create_file_writer(logs_train_dir)
+                optimizer = optimizers.Adam(learning_rate=0.00001)
+                min_error = 1.0
+                step = 0
+                start_time = time.time()
 
-            for i in range(5000000):
-                with tf.GradientTape() as tape:
-                    train = next(itrator)
+                while True:
+                    try:
+                        step += 1
+                        with tf.GradientTape() as tape:
+                            x,y = next(iterator)
+                            out = model(x)
+                            loss = tf.square(out - y)
+                            loss = tf.reduce_sum(loss) / x.shape[0]
+                            print("loss:", loss, "step:", step)
 
-                    x = train[0]
-                    y = train[1]
+                        grads = tape.gradient(loss, model.trainable_variables)
+                        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+                        with summary_writer.as_default():  # 显示tensorboard必须
+                            tf.summary.scalar("loss", float(loss), step=step)
 
-                    out = model(x)
-                    loss = tf.square(out-y)
-                    loss = tf.reduce_sum(loss) / x.shape[0]
-                    print("loss:", loss)
+                        if step%20 == 0:
+                            self.calaccrate(model, train_x, train_y, "error rate training_set:")
+                            error_rate = self.calaccrate(model, valid_x, valid_y, "error rate validation_set:")
+                            if error_rate < min_error:
+                                min_error = error_rate
+                                model.save(models_path + "model_{0}".format(step))
+                                # tf.saved_model.save(model, models_path + "model_{0}".format(step))
+                                if min_error < 0.05:
+                                    break
 
-                grads = tape.gradient(loss, model.trainable_variables)
-                optimizer.apply_gradients(zip(grads, model.trainable_variables))
-                with summary_writer.as_default():       # 显示tensorboard必须
-                    tf.summary.scalar("loss", float(loss), step=i)
-                model.save("/Users/erik/PycharmProjects/Lacomplex/model.5h")
-        else:
-            # print(valid_x[0])
-            model = keras.models.load_model("./CP_ALL/model.5h")
-            out = model(valid_x)
-            # print(out)
+                    except StopIteration:
+                        print("done")
+                        break
 
-            result = []
-            for i in out:
-                if i[0]>i[1]:
-                    result.append(0)
-                else:
-                    result.append(1)
-            print("predict:", len(result))
-            print("predict:", result[250:500])
-            print("ground_t:", valid_y)
-            error_num = 0
-            for p in range(len(result)):
-                if result[p]!=valid_y[p]:
-                    error_num += 1
-            print(error_num/len(result))
+            else:
+                model = tf.saved_model.load("./models/model_20")
+                print("loading model.......done!")
+                self.calaccrate(model, valid_x, valid_y, "error rate validation_set:")
 
 lc = Lacomplex()
 # 0号帧
@@ -395,14 +472,14 @@ lc = Lacomplex()
 
 # lc.batchFrame2Dis()
 # lc.mergeSet()
+# lc.aa_contact()
 # lc.ConDifPerFra(save=True)
 # lc.averageDis()
-
-
+# lc.statistics()
 
 # lc.numperaa()
 # lc.aveDistribution()
 # lc.rmsd()
-lc.extractFeat()
+# lc.extractFeat()
 # lc.mergetrain()
-# lc.train(train_bool=False)
+# lc.train(train_bool=True)
--- print_length.py
+++ print_length.py
@@ -3,22 +3,12 @@ import matplotlib.colors as mcolors
 import numpy as np
 import pandas as pd
 import re
+from tensorflow import keras
+import tensorflow as tf
+
 # print(train1[1499])
 # print(train2[1499])
 
-"""contact1 = np.load("./total_contact_Bi.npy", allow_pickle=True).item()
-contact2 = np.load("./total_contact_No.npy", allow_pickle=True).item()
+model = tf.saved_model.load("./models/model_20")
 
-print(len(contact1),len(contact2))
-total_contact = contact1 | contact2
 
-# print(total_contact)
-np.save("./whole_cp.npy", total_contact)
-
-contact3 = np.load("./whole_cp.npy", allow_pickle=True).item()
-print(len(contact3))"""
-
-li = [1,2,3]
-print(li)
-li[1] = li[1]+4
-print(li)
